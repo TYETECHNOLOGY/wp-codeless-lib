d015caca44af54768a454dcde942d0edfc08dda8
Alessandro Tesoro <mythemesdepot@gmail.com>
2016-06-11 19:07:53 +0200

    Hello world

---
+++ wp-codeless-lib.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Codeless Library.
+ * An helper library with functionalities that are common among my WP projects.
+ *
+ * Copyright (c) 2016 Alessandro Tesoro
+ *
+ * Codeless Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Codeless Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * @author     Alessandro Tesoro
+ * @version    1.0.0
+ * @copyright  (c) 2016 Alessandro Tesoro
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU LESSER GENERAL PUBLIC LICENSE
+ * @package    wp-codeless-lib
+*/
